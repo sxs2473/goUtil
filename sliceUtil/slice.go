37278cfe8bd85dbce181946b9043d33eef158fea
songxinsheng <songxinsheng@didichuxing.com>
2018-08-23 16:29:57 +0800

    add Reversing

--- sliceUtil/slice.go
+++ sliceUtil/slice.go
@@ -42,6 +42,7 @@ func FilterSlice(s []int64, filter func(x int64) bool) []int64 {
 	// 返回的新切片
 	// s[:0] 这种写法是创建了一个 len 为 0，cap 为 len(s) 即和原始切片最大容量一致的切片
 	// 因为是过滤，所以新切片的元素总个数一定不大于比原始切片，这样做减少了切片扩容带来的影响
+	// 同时，也有一个问题，因为 newS 和 s 共享底层数组，那么过滤后 s 也会被修改！
 	newS := s[:0]
 	// 遍历，对每个元素执行 filter，符合条件的加入新切片中
 	for _, x := range s {
@@ -80,4 +81,11 @@ func RemoveDuplicates2(s []int64) []int64 {
 		}
 	}
 	return ret
+}
+
+func Reversing(s []int64) []int64 {
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+		s[left], s[right] = s[right], s[left]
+	}
+	return s
 }
