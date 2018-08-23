7531bee74141d6aa00feec17d8640c3471c2e292
songxinsheng <songxinsheng@didichuxing.com>
2018-08-23 17:11:36 +0800

    add SliceChunk

--- sliceUtil/slice.go
+++ sliceUtil/slice.go
@@ -89,3 +89,13 @@ func Reversing(s []int64) []int64 {
 	}
 	return s
 }
+
+func SliceChunk(s []int64, size int) [][]int64 {
+	var ret [][]int64
+	for size < len(s) {
+		// s[:size:size] 表示 len 为 size，cap 也为 size，第二个冒号后的 size 表示 cap
+		s, ret = s[size:], append(ret, s[:size:size])
+	}
+	ret = append(ret, s)
+	return ret
+}
